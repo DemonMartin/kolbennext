b2a249c0997f12df694b7f0592ce4a897786c997
DemonMartin <skubiduja1981@gmail.com>
2024-01-14 01:01:23 +0100

    Change timer update frequency

--- src/app/components/TimerDisplay.js
+++ src/app/components/TimerDisplay.js
@@ -2,36 +2,36 @@
 import { useState, useEffect } from 'react';
 
 const TimerDisplay = ({ startTime = 0, stop = false }) => {
-    const [displayTime, setDisplayTime] = useState('00:00:00');
+  const [displayTime, setDisplayTime] = useState('00:00:00');
 
-    useEffect(() => {
-        let intervalId;
+  useEffect(() => {
+    let intervalId;
 
-        if (startTime && !stop) {
-            intervalId = setInterval(() => {
-                const now = new Date();
-                const elapsedTime = now - startTime;
-                const hours = String(Math.floor(elapsedTime / 3600000)).padStart(2, '0');
-                const minutes = String(Math.floor((elapsedTime % 3600000) / 60000)).padStart(2, '0');
-                const seconds = String(Math.floor((elapsedTime % 60000) / 1000)).padStart(2, '0');
+    if (startTime && !stop) {
+      intervalId = setInterval(() => {
+        const now = new Date();
+        const elapsedTime = now - startTime;
+        const hours = String(Math.floor(elapsedTime / 3600000)).padStart(2, '0');
+        const minutes = String(Math.floor((elapsedTime % 3600000) / 60000)).padStart(2, '0');
+        const seconds = String(Math.floor((elapsedTime % 60000) / 1000)).padStart(2, '0');
 
-                setDisplayTime(`${hours}:${minutes}:${seconds}`);
-            }, 1000);
-        }
+        setDisplayTime(`${hours}:${minutes}:${seconds}`);
+      }, 100);
+    }
 
-        return () => {
-            if (intervalId) clearInterval(intervalId);
-        };
-    }, [startTime, stop]);
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [startTime, stop]);
 
-    return (
-      <div className="flex justify-center items-center">
-        <div className="text-center p-4 mb-4 rounded-xl shadow-lg bg-white bg-opacity-30 border border-white border-opacity-60">
-          <p className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white">
-            {displayTime}
-          </p>
-        </div>
+  return (
+    <div className="flex justify-center items-center">
+      <div className="text-center p-4 mb-4 rounded-xl shadow-lg bg-white bg-opacity-30 border border-white border-opacity-60">
+        <p className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white">
+          {displayTime}
+        </p>
       </div>
+    </div>
   );
 };
 
