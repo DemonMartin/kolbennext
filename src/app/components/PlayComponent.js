a84295aed729a30144ef9f1ee6ce561e01d51e6f
DemonMartin <skubiduja1981@gmail.com>
2024-01-19 19:33:03 +0100

    Add Average Audio Level Statistics (debug)

--- src/app/components/PlayComponent.js
+++ src/app/components/PlayComponent.js
@@ -12,7 +12,7 @@ export default function PlayComponent({
     const [started, setStarted] = useState(false);
     const [fluidLevel, setFluidLevel] = useState(0);
     const { audioLevel, startRecording, pauseRecording, stopRecording, resetRecording, time, audioURL, recordingState, isRecording, audioFile } = useRecorder();
-	const totalAudioLevel = useState([]);
+    const [totalAudioLevel, setTotalAudioLevel] = useState([0]);
 
     const audioLevelRef = useRef(audioLevel);
     const fluidLevelRef = useRef(fluidLevel);
@@ -34,11 +34,13 @@ export default function PlayComponent({
             intervalIdRef.current = setInterval(() => {
                 console.log(audioLevelRef.current, fluidLevelRef.current);
                 if (fluidLevelRef.current < 1) {
-                    totalAudioLevel.push(audioLevelRef.current);
-                    if (audioLevelRef.current > 12 && fluidLevelRef.current < 1) {
-                        setFluidLevel(Math.min(fluidLevelRef.current + ((audioLevelRef.current / 40000) + 0.003), 1)); // Decrease the step size
+                    setTotalAudioLevel(prevLevels => [...prevLevels, audioLevelRef.current]);
+                    if (audioLevelRef.current > 11 && fluidLevelRef.current < 1) {
+                        const addFluidLevel = (audioLevelRef.current / 40000) + 0.006;
+                        console.log(addFluidLevel);
+                        setFluidLevel(Math.min(fluidLevelRef.current + addFluidLevel, 1)); // Decrease the step size
                     } else if (audioLevelRef.current < 10 && fluidLevelRef.current > 0) {
-                        setFluidLevel(Math.max(fluidLevelRef.current - 0.004, 0)); // Decrease the step size
+                        setFluidLevel(Math.max(fluidLevelRef.current - 0.005, 0)); // Decrease the step size
                     }
                 } else {
                     stopGame();
@@ -62,7 +64,10 @@ export default function PlayComponent({
         <div className={`relative p-4 md:p-6 rounded-3xl shadow-xl transition-shadow duration-300 border border-gray-200 bg-white bg-opacity-25 text-white ${started ? "shadow-2xl" : ""}`}>
             <TimerDisplay startTime={timer} stop={!started} className="text-lg md:text-xl lg:text-2xl xl:text-3xl mb-4 font-semibold text-blue-800" />
             <ImageComponent src={imgSrc} id={id} fluidLevel={fluidLevelRef.current} audioLevel={audioLevel} running={started} />
-            <span>{totalAudioLevel.reduce((a, b) => a + b, 0) / totalAudioLevel.length}</span>
+            {
+                process.env.NODE_ENV !== 'production' && 
+                <span>Average Audio Level: {totalAudioLevel.reduce((a, b) => a + b, 0) / totalAudioLevel.length}</span>
+            }
             <div className="flex justify-center items-center mt-4">
                 <button
                     className={`inline-flex items-center ${started ? 'bg-red-500 hover:bg-red-700' : 'bg-blue-500 hover:bg-blue-700'} text-xs sm:text-sm md:text-lg lg:text-xl xl:text-3xl font-bold rounded-full px-2 sm:px-3 md:px-6 lg:px-8 py-1 sm:py-2 md:py-3 shadow transition duration-150 ease-in-out hover:-translate-y-1 active:translate-y-1`}
